4b335e2a40e56a7bd7aaf976d759be4febb04c68
Amit <a83@Amits-Mac.local>
2019-02-04 17:11:32 +0530

    Initial Commit

---
+++ LocalFramework/LocalFramework.h
@@ -0,0 +1,19 @@
+//
+//  LocalFramework.h
+//  LocalFramework
+//
+//  Created by Amit on 04/02/19.
+//  Copyright © 2019 Josh Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocalFramework.
+FOUNDATION_EXPORT double LocalFrameworkVersionNumber;
+
+//! Project version string for LocalFramework.
+FOUNDATION_EXPORT const unsigned char LocalFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalFramework/PublicHeader.h>
+
+
---
+++ LocalFramework/Service.swift
@@ -0,0 +1,20 @@
+//
+//  Service.swift
+//  LocalFramework
+//
+//  Created by Amit on 04/02/19.
+//  Copyright © 2019 Josh Software. All rights reserved.
+//
+
+import Foundation
+
+public class Service{
+    
+    private init(){
+        
+    }
+    
+    public static func getServiceName() -> String{
+        return "Fucker"
+    }
+}
